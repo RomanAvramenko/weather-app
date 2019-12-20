9001b97f554974806a9d3ce38d73aab2f23ebb69
unknown <ashkaell24@gmail.com>
2019-12-20 12:16:01 +0200

    error-boundary

---
+++ src/components/error-boundary/error-boundary.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './error-boundary.scss'
+
+export default class ErrorBoundary extends React.Component {
+    state = {
+        hasError: false
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true })
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className='error-main'>
+                    <div className='error-image'></div>
+                    <h3 className='error-description'>Guess something went wrong</h3>
+                </div>
+            )
+        }
+        return this.props.children
+    }
+
+}
